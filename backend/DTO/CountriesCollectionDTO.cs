d8f0ece5d9a8ac613e84ce1e0fba314271d50e1f
Noman <baba.noman@live.com>
2023-05-20 22:55:57 +0200

    Added DTO and Model classes as required.

---
+++ backend/DTO/CountriesCollectionDTO.cs
@@ -0,0 +1,7 @@
+using backend.Models;
+namespace backend.DTO;
+
+public class CountriesCollectionDTO
+{
+    public List<Country> Countries {get;set;}
+}
---
+++ backend/DTO/ResponseMessageDTO.cs
@@ -0,0 +1,6 @@
+namespace backend.DTO;
+
+public class ResponseMessageDTO
+{
+    public String Message {get;set;}
+}
